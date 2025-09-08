b57c9b07e9a0bc6239ba28570acb8c11c514577a
theascendantlordsshopify <theascendantlords.shopify@gmail.com>
2025-09-08 22:33:14 +0500

    Updated sidebar.tsx

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,22 +1,25 @@
-@@ -8,6 +8,7 @@ import {
-   CalendarDaysIcon,
-   ChartBarIcon,
-   Cog6ToothIcon,
-+  ShieldCheckIcon,
+import {
+  UserIcon,
+  CalendarDaysIcon,
+  ChartBarIcon,
+  Cog6ToothIcon,
+  ShieldCheckIcon,
   BoltIcon,
- } from '@heroicons/react/24/outline';
- import { ROUTES } from '@/constants';
- import { cn } from '@/lib/utils';
-@@ -42,6 +43,10 @@ const navigation = [
-     name: 'Analytics',
-     href: ROUTES.ANALYTICS,
-     icon: ChartBarIcon,
-+  },
-+  {
-+    name: 'Security',
-+    href: ROUTES.MFA_MANAGE,
-+    icon: ShieldCheckIcon,
-   },
+} from '@heroicons/react/24/outline';
+import { ROUTES } from '@/constants';
+import { cn } from '@/lib/utils';
+
+const navigation = [
+  {
+    name: 'Analytics',
+    href: ROUTES.ANALYTICS,
+    icon: ChartBarIcon,
+  },
+  {
+    name: 'Security',
+    href: ROUTES.MFA_MANAGE,
+    icon: ShieldCheckIcon,
+  },
   {
     name: 'Profile',
     href: ROUTES.PROFILE,
@@ -27,5 +30,9 @@
     href: ROUTES.WORKFLOWS,
     icon: BoltIcon,
   },
-   {
-     name: 'Settings',
+  {
+    name: 'Settings',
+    href: ROUTES.SETTINGS,
+    icon: Cog6ToothIcon,
+  },
+];
